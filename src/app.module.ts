ae7fe09369b1a5d94e877a569d8c60b8873c2293
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-18 19:37:34 -0300

    feat: Adicionei o dotenv na documentação

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/entities/user.entity";
 import { Task } from "./task/entities/task.entity";
 import { TaskModule } from "./task/task.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
+      host: process.env.DB_HOST,
       port: 5432,
-      username: "postgres",
-      password: "12345678",
-      database: "nextauth",
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_USER,
       autoLoadEntities: true,
       entities: [__dirname + "/**/*.entity{.ts,.js}"],
       synchronize: true,
